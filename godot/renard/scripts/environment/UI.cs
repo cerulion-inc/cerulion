f3df97964effa2207f58ae6e2761559916192bbb
Se Hwan Jeon <sehwan@mit.edu>
2024-08-21 15:54:04 -0400

    initial push for refactoring

--- godot/renard/scripts/environment/UI.cs
+++
@@ -1,9 +0,0 @@
-using Godot;
-
-public partial class TestUI : CanvasLayer
-{
-    // Don't forget to rebuild the project so the editor knows about the new signal.
-
-    [Signal]
-    public delegate void StartGameEventHandler();
-}
