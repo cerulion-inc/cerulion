57c0611a23a0ace2b6890124568975f60163667a
se-hwan <sehwan@mit.edu>
2024-08-26 15:36:05 -0400

    adding back easy charts data_ files

--- godot/cerulion/scenes/globals/PointCloudData.gd
+++ godot/cerulion/scenes/globals/PointCloudData.gd
@@ -2,18 +2,18 @@ extends Node
 
 # Size of the circular data
 var data_size: int = 1000
-var data: PackedVector4Array = PackedVector4Array()
+var data: PackedVector3Array
 var head: int = 0
 var tail: int = 0
 var is_full: bool = false
 
 func _ready():
-	# Initialize the data with default Vector4 values
+	# Initialize the data with default Vector3 values
 	data.resize(data_size)
 	for i in range(data_size):
-		data[i] = Vector4.ZERO
+		data[i] = Vector3.ZERO
 
-func enqueue(value: Vector4):
+func enqueue(value: Vector3):
 	data[tail] = value
 	tail = (tail + 1) % data_size
 	
@@ -23,12 +23,12 @@ func enqueue(value: Vector4):
 		if tail == head:
 			is_full = true
 
-func dequeue() -> Vector4:
+func dequeue() -> Vector3:
 	if is_empty():
 		print("Buffer is empty")
-		return Vector4.ZERO
+		return Vector3.ZERO
 	
-	var value: Vector4 = data[head]
+	var value: Vector3 = data[head]
 	head = (head + 1) % data_size
 	is_full = false
 	return value
